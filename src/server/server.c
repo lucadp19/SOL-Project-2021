927003bbdf4a24290852da472a5b4e088aee54f3
lucadp19 <luca.dp1909@gmail.com>
2021-05-17 10:25:59 +0200

    Added first implementation of signal handling
    - Added a sig_handler thread with a very simple implementation.
    - Added a function to yield threads when the server is being terminated.
    - Added executables to .gitignore.

--- src/server/server.c
+++ src/server/server.c
@@ -2,6 +2,8 @@
 
 server_config_t server_config;
 
+static int yield_all_threads(pthread_t sig_handler_tid);
+
 int main(int argc, char* argv[]){ 
     if(argc > 2){
         fprintf(stderr, "There must be at most one additional argument: the path to the config file.\n");
@@ -15,6 +17,40 @@ int main(int argc, char* argv[]){
         perror("Error in reading config file");
         return -1;
     }
+
+    // --- SIGNAL HANDLING --- //
+    pthread_t sig_handler_tid;
+    int* sig_handler_pipe;
+    if( (sig_handler_pipe = malloc(2 * sizeof(int))) == NULL){
+        perror("Error in malloc for sig_handler_pipe");
+        return -1;
+    }
+    if( pipe(sig_handler_pipe) == -1){
+        perror("Error in creating sig_handler_pipe");
+        return -1;
+    }
+
+    if( install_sig_handler(sig_handler_pipe, &sig_handler_tid) == -1){
+        perror("Error in installing signal handler");
+        return -1;
+    }
+
+    // other things
+    
+    if( yield_all_threads(sig_handler_tid) == -1){
+        perror("Error in thread yield");
+        return -1;
+    }
+
+    return 0;
+}
+
+int yield_all_threads(pthread_t sig_handler_tid){
+    int err;
+    if( (err = pthread_join(sig_handler_tid, NULL)) != 0){
+        errno = err;
+        return -1;
+    } 
 
     return 0;
 }
---
+++ src/server/sig_handler.c
@@ -0,0 +1,85 @@
+#include "server.h"
+
+void* sig_handler_thread(void* arg){
+    // install handlers and all 
+    sig_handler_arg_t* sigh_arg = (sig_handler_arg_t*)arg;
+    sigset_t* set_ptr = sigh_arg->set;
+    int* pipe = sigh_arg->pipe;
+
+    #ifdef DEBUG
+        printf("Hello, I'm the handler thread!\n");
+    #endif 
+
+    while(true){
+        int sig;
+        int err = sigwait(set_ptr, &sig);
+        if(err != 0){
+            errno = err;
+            perror("Error in sigwait");
+            return NULL;
+        }
+
+        switch (sig) {
+            case SIGINT:
+            case SIGQUIT:
+                // TODO: terminating now
+                // terminate = true;
+                printf("\nReceived signal, closing server!\n");
+                fflush(stdout);
+                // waking sleeping threads
+                close(pipe[W_ENDP]);
+                return NULL;
+            case SIGHUP:
+                // TODO: terminating *gently*
+                return NULL;
+
+            default: ;
+        }
+    }
+    return NULL;
+}
+
+int install_sig_handler(int* pipe, pthread_t* sig_handler_tid){
+    int err;
+
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    sigaddset(&mask, SIGQUIT);
+
+    if((err = pthread_sigmask(SIG_BLOCK, &mask, NULL)) != 0) {
+        errno = err;
+        return -1;
+    }
+
+    #ifdef DEBUG
+        printf("Masked SIGINT and SIGQUIT\n");
+    #endif
+
+    // Ignoring SIGPIPE
+    struct sigaction sig_act;
+    memset(&sig_act, 0, sizeof(sig_act));
+    sig_act.sa_handler = SIG_IGN;
+    if( (sigaction(SIGPIPE, &sig_act, NULL) == -1)){
+        // perror("Error while trying to ignore SIGPIPE");
+        return -1;
+    }
+
+    #ifdef DEBUG
+        printf("Ignored SIGPIPE\n");
+        fflush(stdout);
+    #endif
+
+    // ------------ SIGHANDLER THREAD ------------ //
+    sig_handler_arg_t sigh_arg;
+    sigh_arg.set  = &mask;
+    sigh_arg.pipe = pipe;
+
+    if( (err = pthread_create(sig_handler_tid, NULL, sig_handler_thread, &sigh_arg)) != 0) {
+        // perror("Error while creating sig_handler thread");
+        errno = err;
+        return -1;
+    }
+    
+    return 0;
+}
