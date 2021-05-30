911e3430701f07c96eebbca8256fe245938c4d08
lucadp19 <luca.dp1909@gmail.com>
2021-05-30 16:26:39 +0200

    Implemented reader-writer lock in LRU

--- src/server/LRU.c
+++ src/server/LRU.c
@@ -18,10 +18,18 @@ int expell_LRU(file_t** expelled_ptr){
         // if current file is the file that called LRU continue
         if(!curr_file->can_be_expelled) continue;
 
+        file_writer_lock(curr_file);
+
         if(least_time == -1 || curr_file->last_use < least_time){
+            if(least_file != NULL) file_writer_unlock(least_file);
+
+            // updating times
             least_time = curr_file->last_use;
             least_file = curr_file;
+        } else { // this file won't be expelled (for now)
+            file_writer_unlock(curr_file);
         }
+
         debug("\t[LRU] least: %lu, current: %lu, curr-name: %s\n",
             least_time, curr_file->last_use, curr_file->path_name);
 
@@ -39,10 +47,13 @@ int expell_LRU(file_t** expelled_ptr){
     free(iter);
 
     logger("[REPLACEMENT] File \"%s\" was removed from the server by the replacement algorithm.\n", (*expelled_ptr)->path_name);
+    logger("[REPLACEMENT] %lu bytes freed.\n", (*expelled_ptr)->path_name);
     return 0;
 }
 
 int expell_multiple_LRU(size_t size_to_free, list_t* expelled_list){
+    if(expelled_list == NULL) return -1;
+
     size_t freed = 0;
 
     // no need for mutex, it has already been locked
@@ -54,7 +65,10 @@ int expell_multiple_LRU(size_t size_to_free, list_t* expelled_list){
         if(expell_LRU(&expelled) == -1)
             break;
 
-        list_push_back(expelled_list, NULL, expelled);
+        if( list_push_back(expelled_list, NULL, expelled) == -1){
+            // no more memory
+            // TODO
+        }
         freed = freed + expelled->size;
     }
 
