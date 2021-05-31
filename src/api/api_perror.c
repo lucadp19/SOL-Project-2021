7021850869b7bc49984b351e8014c8b496088b1b
lucadp19 <luca.dp1909@gmail.com>
2021-05-31 12:36:13 +0200

    Added a couple of forgotten errors to api_perror

--- src/api/api_perror.c
+++ src/api/api_perror.c
@@ -3,15 +3,27 @@
 static const char* errno_to_msg(int err);
 
 void api_perror(const char* str){
-    fprintf(stderr, "%s: %s.", str, errno_to_msg(errno));
+    fprintf(stderr, "%s: %s.\n", str, errno_to_msg(errno));
 }
 
 static const char* errno_to_msg(int err){
     switch (err){
         case 0:
             return "success";
+        case ENOTCONN:
+            return "client is not connected to server";
+        case ENOTEMPTY:
+            return "could not create directory";
+        case ECANCELED:
+            return "could not write every file into directory";
+        case EINVAL:
+            return "cannot write into file: last operation was not a locking-creation";
+        case ENOSYS:
+            return "operation is not implemented yet";
+        case EIO:
+            return "could not open file";
         case ENOTRECOVERABLE:
-            return "fatal server error";
+            return "fatal error in server or client application";
         case EBADE:
             return "fatal error in server-client communication";
         case EEXIST:
